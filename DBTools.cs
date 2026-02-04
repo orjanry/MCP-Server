15afd9dcc111a8d14ae9260d46b41487da81c9c0
Orjan Ryger <orjanry@student.uia.no>
2026-02-04 10:37:21 +0100

    MCP Local Server Setup: Beginning Stage

---
+++ DBTools.cs
@@ -0,0 +1,12 @@
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text.Json;
+
+namespace DefaultNamespace;
+
+[MCPServerToolType]
+public class DBTools
+{
+    
+}
